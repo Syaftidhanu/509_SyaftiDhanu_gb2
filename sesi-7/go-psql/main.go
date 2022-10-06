b98c563c7354a07fe60c209c10c90854e02ff3b7
Syafti <syaftidhanu.safitri@gmail.com>
2022-10-06 12:18:22 +0700

    push-psql

--- sesi-7/go-psql/main.go
+++ sesi-7/go-psql/main.go
@@ -4,9 +4,11 @@ import (
 	"database/sql"
 	"fmt"
 
+	//install modulnya dulu menggunakan go get
 	_ "github.com/lib/pq"
 )
 
+// variable golabl yang digunakan untuk menyimpan seluruh info dari db
 const (
 	host     = "localhost"
 	port     = 5432
@@ -16,21 +18,26 @@ const (
 )
 
 var (
+	//var db memiliki tipe data pointer dari struct sql.db yang akan mereassign dgn obejct dari sql
 	db  *sql.DB
 	err error
 )
 
 func main() {
+	//manggabungkan info dari Psotgresql
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
+	//funsi open yang berasal dari package database/sql
+	//fungsi open hanya berfungsi untuk memvalidasi arg
 	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	//metod ping berguna untuk membangun koneksi ke databse
 	err = db.Ping()
 	if err != nil {
 		panic(err)
@@ -38,7 +45,10 @@ func main() {
 
 	fmt.Println("Successfully connected to database")
 
-	CreateEmployee()
+	// CreateEmployee()
+	// GetEmployess()
+	//UpdateEmployess()
+	DeleteEmployess()
 }
 
 // membuat data employee baru dengan package database/sql
@@ -72,4 +82,69 @@ func CreateEmployee() {
 
 	fmt.Printf("New Employee Data : %+v\n", employee)
 
+}
+
+func GetEmployess() {
+	var results = []Employee{}
+
+	sqlStatement := `SELECT * from employees`
+
+	rows, err := db.Query(sqlStatement)
+
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var employee = Employee{}
+
+		err = rows.Scan(&employee.ID, &employee.Full_name, &employee.Email, &employee.Age, &employee.Division)
+
+		if err != nil {
+			panic(err)
+		}
+
+		results = append(results, employee)
+	}
+
+	fmt.Println("Employee datas:", results)
+}
+
+func UpdateEmployess() {
+	sqlStatement := `
+	UPDATE employees
+	SET full_name = $2, email = $3, division = $4, age = $5
+	WHERE id = $1`
+
+	res, err := db.Exec(sqlStatement, 1, "Airell Jordan Hidayat", "airellhidayat@yahoo.com", "CurDevs", 24)
+
+	if err != nil {
+		panic(err)
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Update Data Amount:", count)
+}
+
+func DeleteEmployess() {
+	sqlStatement := `
+	DELETe from employees
+	WHERE id = $1;
+	`
+
+	res, err := db.Exec(sqlStatement, 1)
+
+	if err != nil {
+		panic(err)
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Deleted Data Amount:", count)
 }
